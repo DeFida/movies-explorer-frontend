a43577a9fba57e3d34c1de5593405b7b97b9e844
DeFida <dasiktazik@gmail.com>
2021-05-09 20:03:24 +0500

    promo and navtab are ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 import Footer from '../Footer/Footer'
+import Main from '../Main/Main'
 
 function App() {
   return (
     <div className="app">
+      <Main />
       <Footer />
     </div>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import './Main.css';
+
+import Promo from '../Promo/Promo'
+
+function Main() {
+  return (
+    <div className="main">
+        <Promo />
+    </div>
+  );
+}
+
+export default Main;
